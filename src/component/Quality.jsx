ffe29a113496238ca0b2bbc017efead13b411d72
Nikhil Singh <98732416+NikhilSinghunited@users.noreply.github.com>
2024-09-14 02:40:58 -0700

    Update Quality.jsx
    Animation remove

--- src/component/Quality.jsx
+++ src/component/Quality.jsx
@@ -1,80 +1,52 @@
 import React from 'react';
-import { motion } from 'framer-motion';
 import './Quality.css';
 
 const Quality = () => {
-  const containerVariants = {
-    hidden: {},
-    visible: {
-      transition: {
-        staggerChildren: 0.3,
-      },
-    },
-  };
-
-  const textVariants = {
-    hidden: { opacity: 1, y: 50 },
-    visible: { opacity: 1, y: 0 },
-  };
-
-  const valueVariantsLeft = {
-    hidden: { opacity: 1, x: -50 },
-    visible: { opacity: 1, x: 0 },
-  };
-
-  const valueVariantsRight = {
-    hidden: { opacity: 1, x: 50 },
-    visible: { opacity: 1, x: 0 },
-  };
-
   return (
-    <section className="section">
-      <motion.div
-        className="section-content"
-        initial="hidden"
-        whileInView="visible"
-        viewport={{ once: false, amount: 0.5 }}
-        variants={containerVariants}
-      >
-        <motion.h2 variants={textVariants}>Our Core Values</motion.h2>
-        <motion.p className="main-text" variants={textVariants}>
-        Discover what makes us stand out in the industry
-        </motion.p>
-        <div className="values">
-          <motion.div className="value" variants={valueVariantsLeft}>
+    <section className='section'>
+      <div className='section-content'>
+        <h2>Our Core Values</h2>
+        <p className='main-text'>
+          Discover what makes us stand out in the industry
+        </p>
+        <div className='values'>
+          <div className='value'>
             <h3>Quality Standards</h3>
             <p>
-            We uphold the highest standards of quality in our steel products, ensuring reliability and durability.
+              We uphold the highest standards of quality in our steel products,
+              ensuring reliability and durability.
             </p>
-          </motion.div>
-          <motion.div className="value" variants={valueVariantsRight}>
+          </div>
+          <div className='value'>
             <h3>Innovative Solutions</h3>
             <p>
-            We continually innovate to deliver cutting-edge steel products that meet market needs.
+              We continually innovate to deliver cutting-edge steel products
+              that meet market needs.
             </p>
-          </motion.div>
-          <motion.div className="value" variants={valueVariantsLeft}>
+          </div>
+          <div className='value'>
             <h3>Customer Commitment</h3>
             <p>
-            Customer satisfaction is our priority. We provide personalized service and customized steel solutions.
+              Customer satisfaction is our priority. We provide personalized
+              service and customized steel solutions.
             </p>
-          </motion.div>
-
-          <motion.div className="value" variants={valueVariantsLeft}>
+          </div>
+          <div className='value'>
             <h3>24 X 7 Support</h3>
             <p>
-            We offer round-the-clock support to meet our clients' needs promptly.
+              We offer round-the-clock support to meet our clients' needs
+              promptly.
             </p>
-          </motion.div>
-
-          <motion.div className="value" variants={valueVariantsLeft}>
+          </div>
+          <div className='value'>
             <h3>Eco-Friendly Practices</h3>
             <p>
-            We prioritize sustainability with eco-friendly materials and processes.
+              We prioritize sustainability with eco-friendly materials and
+              processes.
             </p>
-          </motion.div>
+          </div>
         </div>
-      </motion.div>
+      </div>
     </section>
   );
 };
